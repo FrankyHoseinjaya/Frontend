aadea87ddc97d13cbebca5439acc16be4b2e15d8
FrankyHoseinjaya <76642502+FrankyHoseinjaya@users.noreply.github.com>
2021-05-30 22:06:01 +0700

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  //untuk merubah port
   server : {
     port : 8080
   }
